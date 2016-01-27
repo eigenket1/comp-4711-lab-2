26732b0f1996c18e9bacf883227de8e367390525
Hank Lo <Hlo1453@gmail.com>
2016-01-26 16:45:25 -0800

    Added htaccess, controllers and views for about gallery and welcome

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,13 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class About extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
 	 */
 	public function index()
 	{
-		$this->load->view('welcome');
+		$this->load->view('about');
 	}
 }
--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -1,13 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Gallery extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
 	 */
 	public function index()
 	{
-		$this->load->view('welcome');
+		$this->load->view('gallery');
 	}
 }
